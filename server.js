3c4469851516d8da0225495ab7c9b6a69142bb9a
ElK <62822011+evkonradi@users.noreply.github.com>
2020-07-31 17:48:10 -0500

    html routes are created

---
+++ server.js
@@ -0,0 +1,17 @@
+//const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+const express = require('express');
+const {notes} = require('./db/db');
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+//app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+app.use(express.static('public'));
+
+app.listen(PORT, ()=>{
+    console.log(`API server now on port ${PORT}`);
+});
